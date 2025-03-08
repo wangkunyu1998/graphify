e7adf2163c42d9457da357eb757b137297637d1b
王昆宇 <wangkunyu@social-touch.com>
2025-03-08 16:33:50 +0800

    feat: 修改导出属性

--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import Line from '@/components/Line';
 import Sector from '@/components/Sector';
 import animation from '@/utils/animation'
 import Stage from '@/stage.js';
-// const root = document.getElementById('app')
-// console.log(root)
-// const stage = new Stage(root);
-// const ctx = stage.getCtx();
-
-// const rect1 = new Rect(stage, { x: 10, y: 10, width: 100, height: 100, bgColor: 'red', borderColor: 'green', borderWidth: 6, radius: 20, zIndex: 0 })
-// stage.appendChild(rect1)
-console.log(1)
-export {
+const graphifyChart = {
   Rect,
   Circle,
   Line,
   Sector,
-  animation
+  animation,
+  Stage
 }
-export default Stage
+export default graphifyChart
